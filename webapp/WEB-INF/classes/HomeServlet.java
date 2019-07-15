91ee166a02605b8c75865a76f766ebde33129dc1
Nathalie Bize <nathaliebize@gmail.com>
2019-07-15 00:04:27 -0700

    Format and add JavaDoc in HomeServlet file

--- webapp/WEB-INF/classes/HomeServlet.java
+++ webapp/WEB-INF/classes/HomeServlet.java
@@ -1,22 +1,37 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.annotation.WebServlet;
 
+/**
+* HomeServlet is a simple servlet that greet any user.
+*
+* @author Nathalie Bize
+*/
 @WebServlet(urlPatterns={"/"})
 public class HomeServlet extends HttpServlet 
 {
+    /**
+    * This method handles a get method.
+    *
+    * @param request the request object
+    * @param response the response object
+    * @throws ServletException
+    * @throws IOException
+    */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException 
     {
         response.setContentType("text/html"); 
         PrintWriter out = response.getWriter();
         out.println(
-            "<html><head><title>Hello Client!</title></head><body>"
-            + "<h1>Hello Sphynx!</h1>"
-            + "</body></html>" );
+            "<html>" +
+            "<head><title>Hello Client!</title></head>" +
+            "<body><h1>Hello Sphynx!</h1></body>" +
+            "</html>" 
+            );
     }
 }
