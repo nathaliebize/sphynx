fb036e1b265013eb1eae873475158523fa04a40c
Nathalie Bize <nathaliebize@gmail.com>
2019-07-26 14:01:54 -0700

    Update structures to use spring

---
+++ src/main/java/com/nathaliebize/SphynxApplication.java
@@ -0,0 +1,13 @@
+package com.nathaliebize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SphynxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SphynxApplication.class, args);
+	}
+
+}
