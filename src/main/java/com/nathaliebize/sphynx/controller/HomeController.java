68f2d7e4e6593cc2ed6b41d31f322aeb015c7a46
Nathalie Bize <nathaliebize@gmail.com>
2019-08-01 21:19:17 -0700

    Add javadoc to home and users controllers

--- src/main/java/com/nathaliebize/sphynx/controller/HomeController.java
+++ src/main/java/com/nathaliebize/sphynx/controller/HomeController.java
@@ -3,22 +3,21 @@ package com.nathaliebize.sphynx.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller that handles home page
+ *
+ * @author Nathalie Bize
+ *
+ */
 @Controller
 public class HomeController {
     
+    /**
+     * Home page get request handler
+     * @return the index template
+     */
     @GetMapping("/")
     public String showHomePage() {
         return "index";
     }
-    
-    @GetMapping("/terms")
-    public String showTermsPage() {
-	return "terms";
-    }
-    
-    @GetMapping("/sessions")
-    public String showSessionsPage() {
-	return "sessions";
-    }
-
 }
--- src/main/java/com/nathaliebize/sphynx/controller/UserController.java
+++ src/main/java/com/nathaliebize/sphynx/controller/UserController.java
@@ -2,16 +2,20 @@ package com.nathaliebize.sphynx.controller;
 
 import com.nathaliebize.sphynx.model.User;
 import com.nathaliebize.sphynx.repository.UserRepository;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller that handles all user's views
+ * 
+ * @author Nathalie Bize
+ *
+ */
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -19,52 +23,74 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
     
+    /**
+     * Handles login get request
+     * @param model
+     * @param request
+     * @return the user/login template
+     */
     @GetMapping("/login")
     public String showLoginPage(Model model, HttpServletRequest request) {
-	model.addAttribute("user", new User());
-	String error = (String) request.getSession().getAttribute("error");
-	if (error != null) {
-	    model.addAttribute("error", error);
-	}
-	return "user/login";
+        model.addAttribute("user", new User());
+        String error = (String) request.getSession().getAttribute("error");
+        if (error != null) {
+            model.addAttribute("error", error);
+        }
+        return "user/login";
     }
     
+    /**
+     * Handles login post request
+     * @param user
+     * @return the sites template
+     */
     @PostMapping("/login")
     public String login(@ModelAttribute User user) {
-	User temp = userRepository.findByEmail(user.getEmail());
-	if (temp == null) {
-	    return "redirect:register";
-	}
-	return "sites";
+        User temp = userRepository.findByEmail(user.getEmail());
+        if (temp == null) {
+            return "redirect:register";
+        }
+        return "sites";
     }
     
+    /**
+     * Handles register get request
+     * @param model
+     * @return user/register template
+     */
     @GetMapping("/register")
     public String showRegisterPage(Model model) {
-	model.addAttribute("user", new User());
-	return "user/register";
+        model.addAttribute("user", new User());
+        return "user/register";
     }
     
+    /**
+     * Handles register post request
+     * @param model
+     * @param user
+     * @param bindingResult
+     * @return sites template if data are valid, user/register template otherwise
+     */
     @PostMapping("/register")
     public String register(Model model, @Valid @ModelAttribute User user, BindingResult bindingResult) {
-	if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             return "user/register";
         }
-	if (userRepository.findByEmail(user.getEmail()) == null) {
-	    userRepository.save(user);
-	    return "sites";
-	} else {
-	    model.addAttribute("error", "email already used");
-	    return "user/register";
-	}
-
-	
+        if (userRepository.findByEmail(user.getEmail()) == null) {
+            userRepository.save(user);
+            return "sites";
+        } else {
+            model.addAttribute("error", "email already used");
+            return "user/register";
+        }
     }
     
+    /**
+     * Handles logout get request
+     * @return redirection to home page
+     */
     @GetMapping("/logout")
     public String logout() {
-	return "redirect:/";
+        return "redirect:/";
     }
-    
-    
-
 }
