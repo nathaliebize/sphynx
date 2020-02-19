3ae0a68e9f4e215b4fb9c5d9124779b23d5ce5ab
Nathalie Bize <nathaliebize@gmail.com>
2020-02-19 12:44:08 -0800

    Create activity to eventype enum.

--- src/main/java/com/nathaliebize/sphynx/model/EventType.java
+++ src/main/java/com/nathaliebize/sphynx/model/EventType.java
@@ -1,5 +1,23 @@
 package com.nathaliebize.sphynx.model;
 
 public enum EventType {
-    CLICK, INACTIVITY, KEYDOWN, LEAVE_TAB, RETURN_TAB, READING, VIEW_FIFTY, VIEW_ONE_HUNDRED, START, QUIT, CHANGE_URL;
+     
+    CHANGE_URL("Opened new page"),
+    CLICK("Clicked"),
+    INACTIVE("Stopped interaction"),
+    KEYDOWN("Typed"), 
+    LEAVE_TAB("Left tab"),
+    RETURN_TAB("Returned to tab"),
+    START("Started session"),
+    VIEW_ONE_HUNDRED("Reached end of the document");
+    
+    private String activity;
+    
+    private EventType(String activity) {
+        this.activity = activity;
+    }
+    
+    public String getActivity() {
+        return activity;
+    }
 }
