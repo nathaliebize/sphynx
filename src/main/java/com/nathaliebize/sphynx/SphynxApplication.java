86650c4688e5c51e153cfe9f7563908f54ab8f58
Nathalie Bize <nathaliebize@gmail.com>
2020-02-19 10:56:38 -0800

    Add extension servlet initializer to sphynxApplication class. Rename files

--- src/main/java/com/nathaliebize/sphynx/SphynxApplication.java
+++ src/main/java/com/nathaliebize/sphynx/SphynxApplication.java
@@ -2,14 +2,18 @@ package com.nathaliebize.sphynx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/***
+ * This is the starting point for Sphynx.
+ */
 @SpringBootApplication
 @EnableJpaAuditing
-public class SphynxApplication {
+public class SphynxApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-	    SpringApplication.run(SphynxApplication.class, args);
+	    SpringApplication.run(SphynxApplication.class, args);	    
 	}
 
 }
